0ff53d979b287b02e61c28e858ddc9ffbd80f3c2
xhu <xhu@freewheel.tv>
2017-12-20 16:00:40 +0800

    add useful scripts and remove package-json

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 const gulp = require('gulp'),
-      babel = require('gulp-babel'),
-      uglify = require('gulp-uglify'),
-      minifyCss = require('gulp-minify-css'),
-      concat = require('gulp-concat'),
-      concatCss = require('gulp-concat-css'),
-      rename = require('gulp-rename');
+  babel = require('gulp-babel'),
+  uglify = require('gulp-uglify'),
+  cleanCss = require('gulp-clean-css'),
+  concat = require('gulp-concat'),
+  concatCss = require('gulp-concat-css'),
+  rename = require('gulp-rename');
 
 const jsFiles = [
   './app/assets/javascripts/util.js',
@@ -16,18 +16,18 @@ gulp.task('js', function () {
   gulp.src(jsFiles)
     .pipe(concat('all.min.js'))
     .pipe(gulp.dest('./app/assets/dist'))
-    .pipe(babel({ presets: ['es2015'] }))
+    .pipe(babel({ presets: ['env'] }))
     .pipe(uglify())
     .pipe(gulp.dest('./app/assets/dist'));
 });
 
 gulp.task('css', function () {
   gulp.src([
-      './app/assets/stylesheets/layout.css',
-      './app/assets/stylesheets/*.css'
-    ])
+    './app/assets/stylesheets/layout.css',
+    './app/assets/stylesheets/*.css'
+  ])
     .pipe(concatCss('all.min.css'))
-    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(cleanCss({ compatibility: 'ie8' }))
     .pipe(gulp.dest('./app/assets/dist'));
 });
 
--- scripts/create-post.js
+++ scripts/create-post.js
@@ -1,3 +1,5 @@
+const { resolve } = require('path');
+const { readdirSync, open, close } = require('fs');
 const { createInterface } = require('readline');
 
 const rl = createInterface({
@@ -6,37 +8,60 @@ const rl = createInterface({
 });
 
 const getSequence = post => new Promise(res => {
-  rl.question('The sequence of the post:\n[Default: 95]', seq => {
-    console.log();
-    post.seq = seq;
+  const nextSeq = readdirSync(resolve(__dirname, '../archives')).filter(
+    file => file.endsWith('.md')
+  ).map(file => parseInt(/\d+/.exec(file.split('*')[0])[0])).sort(
+    (a, b) => a > b ? -1 : 1
+  )[0] + 1;
+
+  rl.question(`The sequence of the post:\n[Default: ${nextSeq}] `, seq => {
+    seq = seq.trim();
+    post.seq = seq && parseInt(seq) ? seq : nextSeq;
     res(post);
   });
 });
 
 const getTitle = post => new Promise(res => {
-  rl.question('The title of the post:\n', title => {
-    console.log();
-    post.title = title;
+  rl.question('The title of the post:\n[Default: Placeholder] ', title => {
+    title = title.trim();
+    post.title = title.length ? title : 'Placeholder';
     res(post);
   });
 });
 
 const getDate = post => new Promise(res => {
-  rl.question('The date when you write the post:\n[Default: 20171220]', date => {
-    console.log();
-    post.date = date;
+  const date = new Date();
+  const nowDate = `${date.getUTCFullYear()}${date.getUTCMonth() + 1}${date.getUTCDate()}`;
+  const validateDate = str => (
+    8 === str.length &&
+    parseInt(str.slice(0, 4)) &&
+    parseInt(str.slice(4, 6)) && parseInt(str.slice(4, 6)) <= 12 &&
+    parseInt(str.slice(6)) && parseInt(str.slice(6)) <= 31
+  );
+
+  rl.question(`The date when you write the post:\n[Default: ${nowDate}] `, date => {
+    date.trim();
+    post.date = validateDate(date) ? date : nowDate;
     res(post);
   });
 });
 
 const getTags = post => new Promise(res => {
-  rl.question('The tags of the post:\n', tags => {
-    console.log();
-    post.tags = tags.split(' ');
+  rl.question('The tags of the post:\n[Default: Placeholder] ', tags => {
+    tags = tags.trim();
+    post.tags = tags.length ? tags.split(' ') : ['Placeholder'];
     res(post);
     rl.close();
   });
 });
+
+const touchFile = post => {
+  const { seq, title, date, tags } = post;
+  const fileName = 'WIP: ' + [
+    seq, title, date, tags.join('-')
+  ].join('*') + '.md';
+  open(resolve(__dirname, '../archives/', fileName), 'w', (_, file) => close(file, () => {}));
+};
 
 getSequence({}).then(
   post => getTitle(post)
@@ -45,5 +70,5 @@ getSequence({}).then(
 ).then(
   post => getTags(post)
 ).then(
-  (post) => console.log(post)
+  post => touchFile(post)
 );
