97e13e79d39de007ce9753a0b0f835a6da48c5c6
MrHuxu <hxtheone@gmail.com>
2023-04-06 15:33:57 +0800

    finish error page, handle errors

--- api/index.go
+++ api/index.go
@@ -13,24 +13,37 @@ import (
 func Index(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := templates.GetTemplate()
 	if err != nil {
-		log.Fatal(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
 	}
 
-	posts, _, err := posts.GetPosts()
+	var data []*posts.Post
+	defer func() {
+		if err != nil {
+			tmpl.Execute(w, map[string]any{
+				"page":    "error",
+				"title":   "Error",
+				"message": err.Error(),
+			})
+			return
+		}
+
+		tmpl.Execute(w, map[string]any{
+			"page":        "index",
+			"title":       "Life of xhu",
+			"selectedTag": r.URL.Query().Get("tag"),
+			"posts":       data,
+		})
+	}()
+	data, _, err = posts.GetPosts()
 	if err != nil {
 		log.Fatal(err)
 	}
 	if r.URL.Query().Has("tag") {
-		posts = filterPostsByTag(posts, r.URL.Query().Get("tag"))
+		data = filterPostsByTag(data, r.URL.Query().Get("tag"))
 	}
-	sort.Slice(posts, func(i, j int) bool { return posts[i].ID > posts[j].ID })
-
-	tmpl.Execute(w, map[string]any{
-		"page":        "index",
-		"title":       "Life of xhu",
-		"selectedTag": r.URL.Query().Get("tag"),
-		"posts":       posts,
-	})
+	sort.Slice(data, func(i, j int) bool { return data[i].ID > data[j].ID })
 }
 
 func filterPostsByTag(original []*posts.Post, selectedTag string) []*posts.Post {
--- api/post.go
+++ api/post.go
@@ -2,13 +2,14 @@ package api
 
 import (
 	"bytes"
+	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 
 	"github.com/MrHuxu/blogo/posts"
 	"github.com/MrHuxu/blogo/templates"
+
 	"github.com/yuin/goldmark"
 )
 
@@ -16,29 +17,48 @@ import (
 func Post(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := templates.GetTemplate()
 	if err != nil {
-		log.Fatal(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
 	}
 
+	var data *posts.Post
+	defer func() {
+		if err != nil {
+			tmpl.Execute(w, map[string]any{
+				"page":    "error",
+				"title":   "Error",
+				"message": err.Error(),
+			})
+			return
+		}
+
+		tmpl.Execute(w, map[string]any{
+			"page":  "post",
+			"title": fmt.Sprintf("Life of xhu - %s", data.Title),
+			"post":  data,
+		})
+	}()
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil {
-		log.Fatal(err)
+		return
 	}
 
 	_, mapIDToPosts, err := posts.GetPosts()
 	if err != nil {
-		log.Fatal(err)
+		return
 	}
-	post := mapIDToPosts[id]
+	if data = mapIDToPosts[id]; data == nil {
+		err = fmt.Errorf("Post[id=%d] not found", id)
+		return
+	}
 
-	post.LoadContent()
+	if err = data.LoadContent(); err != nil {
+		return
+	}
 	var buf bytes.Buffer
-	if err := goldmark.Convert([]byte(post.Content), &buf); err != nil {
+	if err := goldmark.Convert([]byte(data.Content), &buf); err != nil {
 		panic(err)
 	}
-	post.ContentHTML = template.HTML(buf.String())
-
-	tmpl.Execute(w, map[string]any{
-		"page": "post",
-		"post": post,
-	})
+	data.ContentHTML = template.HTML(buf.String())
 }
